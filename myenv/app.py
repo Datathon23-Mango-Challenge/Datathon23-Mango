b7f457bc27f3c517debd342e7fdbf7772cc96ca0
pmorente <pau.morente@estudiantat.upc.edu>
2023-11-12 11:55:48 +0100

    commit

--- myenv/app.py
+++ myenv/app.py
@@ -1,33 +1,67 @@
 from flask import Flask, render_template
 import pandas as pd
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def show_images():
     data = pd.read_csv('./static/csvs/merged_data.csv')
-    # Asegurarse de que hay suficientes datos para la muestra
-    if len(data) < 8:
-        return "No hay suficientes datos para mostrar las imágenes."
+
 
     main_images = data.sample(n=8)  # Asumimos que esta muestra tiene las columnas necesarias
+
+
+# ... (código previo)
+
 
     images_info = {}
     for _, main_row in main_images.iterrows():
         # Filtrar imágenes con el mismo 'cod_outfit', excluyendo la imagen principal
-        related_by_outfit = data[(data['cod_outfit'] == main_row['cod_outfit']) & (data['des_filename'] != main_row['des_filename'])]
+        related_images = data[
+            (data['cod_outfit'] == main_row['cod_outfit']) &
+            (data['des_filename'] != main_row['des_filename'])
+        ].drop_duplicates(subset=['des_product_aggregated_family', 'des_product_family', 'des_product_type'])
 
-        # Filtrar imágenes con el mismo 'des_agrup_color_eng' y diferente 'des_product_aggregated_family'
-        related_by_color_family = data[(data['des_agrup_color_eng'] == main_row['des_agrup_color_eng']) & 
-                                       (data['des_product_aggregated_family'] != main_row['des_product_aggregated_family']) & 
-                                       (data['cod_outfit'] != main_row['cod_outfit'])]
 
-        # Concatenar ambos conjuntos, eliminar duplicados y tomar una muestra de 6 imágenes
-        related_images = pd.concat([related_by_outfit, related_by_color_family]).drop_duplicates('des_filename').sample(n=6, replace=True)
+        # Garantizar que haya al menos un 'Bottoms' y un 'Tops'
+        bottoms = data[(data['des_product_category'] == "Bottoms") & (~data['des_filename'].isin(related_images['des_filename']))]
+        tops = data[(data['des_product_category'] == "Tops") & (~data['des_filename'].isin(related_images['des_filename']))]
 
-        images_info[main_row['des_filename']] = related_images['des_filename'].tolist()
+
+        # Si no hay suficientes imágenes relacionadas, buscar más imágenes por color
+        if len(related_images) < 4:  # Dejamos espacio para 2 prendas adicionales
+            additional_images = data[
+                (data['des_agrup_color_eng'] == main_row['des_agrup_color_eng']) &
+                (~data['des_filename'].isin(related_images['des_filename'])) &
+                (data['des_product_category'] != "Bottoms") &  # Excluir 'Bottoms'
+                (data['des_product_category'] != "Tops")      # Excluir 'Tops'
+            ].drop_duplicates(subset=['des_product_aggregated_family', 'des_product_family', 'des_product_type'])
+
+
+            related_images = pd.concat([related_images, additional_images]).drop_duplicates(subset=['des_product_family', 'des_product_type']).head(4)
+
+
+            # Añadir al menos un 'Bottoms' y un 'Tops'
+           
+            if bottoms.empty or tops.empty:
+                    # Manejo de casos donde no hay disponibles suficientes 'Bottoms' o 'Tops'
+                pass
+            else:
+                additional_images = pd.concat([additional_images, bottoms.sample(1), tops.sample(1)])
+
+
+       
+        images_info[main_row['des_filename']] = related_images.head(6)['des_filename'].tolist()
+
 
     return render_template('index.html', images_info=images_info)
 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
